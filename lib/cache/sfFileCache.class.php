6f90a32db7cb14a114b7f6c1b9d964607ea72bda
Antti Hätinen <pharazon@phz.fi>
2015-12-26 03:20:42 +0200

    set_magic_quotes_runtime is deprecated in PHP 5.3

--- lib/cache/sfFileCache.class.php
+++ lib/cache/sfFileCache.class.php
@@ -480,15 +480,19 @@ class sfFileCache extends sfCache
     {
       clearstatcache(); // because the filesize can be cached by PHP itself...
       $length = @filesize($path.$file);
-      $mqr = get_magic_quotes_runtime();
-      set_magic_quotes_runtime(0);
+      if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+        $mqr = get_magic_quotes_runtime();
+        set_magic_quotes_runtime(0);
+      }
       if ($this->readControl)
       {
         $hashControl = @fread($fp, 32);
         $length = $length - 32;
       }
       $data = ($length) ? @fread($fp, $length) : '';
-      set_magic_quotes_runtime($mqr);
+      if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+        set_magic_quotes_runtime($mqr);
+      }
       if ($this->fileLocking)
       {
         @flock($fp, LOCK_UN);
